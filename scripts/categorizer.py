5bb1653b8b04d0d616dd6d82c968577b56d52d68
Charles Cain <charlesbcain@gmail.com>
2019-04-29 07:32:17 -0600

    Added a categorizer for empty lines, and lists to represent what we want to categorize in the future

--- scripts/categorizer.py
+++ scripts/categorizer.py
@@ -1,5 +1,18 @@
 
 
+def find_empty_lines(line):
+    """
+    Returns 1 for an empty line and 0 for non-empty
+    :param line:
+    :return:
+    """
+    if line.strip("\t").strip(" ").strip("\n") == '':
+        return 1
+    else:
+        return 0
+
+
+
 def main():
     """
     Currently this function does some prescripted stuff.
@@ -10,11 +23,23 @@ def main():
     with open("./categorizer.py") as fp:
         lines = fp.readlines()
 
+    empty_lines = []
+    comment_lines = []
+    assigment_lines = []
+    conditional_lines = []
+    control_lines = []
+    indent_levels = []
+    function_call_lines = []
+    function_def_lines = []
+
+    # Categorizing lines
+    for line in lines:
+        empty_lines.append(find_empty_lines(line))
 
 
     # Debug portion
-    empty_category = [0] * len(lines)
-    print_all_cats(lines, empty_category)
+    print("Empty Lines")
+    print_all_cats(lines, empty_lines)
 
 def print_all_cats(lines, category):
     """
@@ -27,7 +52,7 @@ def print_all_cats(lines, category):
 
     for line_number, line in enumerate(lines):
 
-        print(category[line_number] + " " + line)
+        print(str(category[line_number]) + " " + line.strip("\n"))
 
 
 if __name__ == "__main__":
